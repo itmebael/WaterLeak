230f7b80e0d8bf5ed05eeeb94d466482f0a99e53
itmebael <abelredoblado22@gmail.com>
2026-01-27 16:10:56 +0800

    Update Flutter web build for Vercel deployment

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -3,7 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "6e612648e00e7f534e6be3d9d5b3e5ec",
+const RESOURCES = {".vercel/project.json": "2d44407a7f04440d1047790600b256e1",
+".vercel/README.txt": "2b13c79d37d6ed82a3255b83b6815034",
+"assets/AssetManifest.bin": "6e612648e00e7f534e6be3d9d5b3e5ec",
 "assets/AssetManifest.bin.json": "7dadc28bbd61e76bff6bb6e3025576f5",
 "assets/AssetManifest.json": "8e8c13092708a6caa0b378735dbff309",
 "assets/assets/images/background.png": "97bcdff34666d1b3e9ccbac7b1a58d95",
@@ -30,14 +32,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "6e612648e00e7f534e6be3d9d5b3e5ec
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "35e7e308f3bcca9a4d18270eee795c4f",
+"flutter_bootstrap.js": "96ce4d62661866071d012f99adb51000",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "81bbf81381c1b446ad52a85efd4c7555",
 "/": "81bbf81381c1b446ad52a85efd4c7555",
-"main.dart.js": "ac39396c73f91b2175940eb68ca4eeb2",
+"main.dart.js": "cdbcebcd89482df4a9891a52982b761c",
 "manifest.json": "5072ffc0dfb015663ee4a242f255696d",
 "version.json": "93cfe22b81554710ac10d809e1af36bf"};
 // The application shell files that are downloaded before a service worker can
